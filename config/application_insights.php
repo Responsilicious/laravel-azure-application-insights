decd44f92c2ea535e29c1c8c27de3de171ee51c0
WillHazell <will.hazell10@gmail.com>
2023-11-01 11:46:24 +0000

    Update application_insights.php
    add config for cloud role name and instance ID

--- config/application_insights.php
+++ config/application_insights.php
@@ -14,4 +14,11 @@ return [
      * Whether to track users (anonymously)
      */
     'track_anonymous_users' => env('APPINSIGHTS_TRACK_ANONYMOUS_USERS', true),
+
+    /**
+     * Set naming for cloud role and instance
+     */
+    'cloud_role_name' => env('WEBSITE_SITE_NAME'),
+    
+    'cloud_role_instance' => env('WEBSITE_INSTANCE_ID')
 ];
